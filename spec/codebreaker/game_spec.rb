c9cd425c72b11a722e447fdc281cf17d4a0528dd
cpeak <cpeak66@gmail.com>
2013-08-18 13:34:15 -0500

    Refactored the guess function to better communicate intent

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -25,6 +25,22 @@ module Codebreaker
           game.guess('5554')
         end
       end
+
+      context "with 1 number match" do
+        it "sends a mark with '-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+
+      context "with 1 exact match" do
+        it "sends a mark with '+'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
+      end
     end
   end
 end
